fe3c0210e6e53f23bf89403f3f3d3aba2b8620e0
umeh-promise <umehpromise1000@gmail.com>
2025-09-30 02:19:37 +0100

    feat: add reuseable button

---
+++ lib/core/widgets/button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:trust_services_app/core/theme/colors.dart';
+
+class Button extends StatelessWidget {
+  final Color backgroundColor;
+  final Color color;
+  final String buttonText;
+  final VoidCallback onPressed;
+
+  const Button(
+    this.buttonText, {
+    super.key,
+    required this.onPressed,
+    this.backgroundColor = AppPallete.primaryColor,
+    this.color = AppPallete.whiteColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(backgroundColor: backgroundColor),
+        child: Text(
+          buttonText,
+          style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+            color: color,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+    );
+  }
+}
