243ad285735ac3b8bc95be07298c1d78c6b42629
umeh-promise <umehpromise1000@gmail.com>
2025-09-30 02:19:52 +0100

    feat: add splash screen

---
+++ lib/features/home/pages/splash.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:trust_services_app/core/theme/colors.dart';
+import 'package:trust_services_app/core/widgets/button.dart';
+import 'package:trust_services_app/features/home/widgets/carousel.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Image.asset(
+            './assets/img/repairmen.png',
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+            color: AppPallete.bgOverlayColor,
+            colorBlendMode: BlendMode.darken,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              left: 24,
+              top: 18,
+              right: 24,
+              bottom: 72,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              spacing: 16,
+              children: [
+                Carousel(),
+                Button('Get Started', onPressed: () {}),
+                Button(
+                  'Login Now',
+                  onPressed: () {},
+                  backgroundColor: AppPallete.whiteColor,
+                  color: AppPallete.primaryColor,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/features/home/widgets/carousel.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_carousel_widget/flutter_carousel_widget.dart';
+import 'package:trust_services_app/core/theme/colors.dart';
+
+final List<Map<String, String>> _carouselItems = [
+  {
+    "title": "Best in providing fast job services",
+    "subtitle":
+        "We work with verified job service providers.\nWhat are you looking for? Professional plumbers, hairstylists, electricians, etc.",
+  },
+  {
+    "title": "Trusted professionals for every service",
+    "subtitle":
+        "From home repairs to beauty experts — book verified professionals near you.",
+  },
+  {
+    "title": "Fast, reliable, and affordable",
+    "subtitle": "Get services done by experts who value your time and money.",
+  },
+];
+
+class Carousel extends StatelessWidget {
+  const Carousel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterCarousel(
+      options: FlutterCarouselOptions(
+        autoPlay: true,
+        autoPlayInterval: const Duration(seconds: 3),
+        height: 275,
+        viewportFraction: 1.0,
+        enableInfiniteScroll: true,
+        indicatorMargin: 7,
+        slideIndicator: CircularSlideIndicator(
+          slideIndicatorOptions: SlideIndicatorOptions(
+            currentIndicatorColor: AppPallete.primaryColor,
+            indicatorBackgroundColor: AppPallete.whiteColor,
+          ),
+        ),
+        reverse: true,
+      ),
+      items: _carouselItems
+          .map(
+            (item) => Column(
+              spacing: 14,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  item['title'] as String,
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: AppPallete.whiteColor,
+                    letterSpacing: 1.2,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '${item['subtitle']}',
+                  style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: AppPallete.whiteColor,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          )
+          .toList(),
+    );
+  }
+}
